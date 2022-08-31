4f204d97466c7c868e7f777ce64def6e9fb911de
KaterinaVis <katerina4484@mail.ru>
2022-08-31 22:58:26 +0500

    ex47

--- ex47/Program.cs
+++ ex47/Program.cs
@@ -10,25 +10,25 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите n");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int [,] matr = FillMatrix(m,n,-10,10);
+double [,] matr = FillMatrix(m,n);
 PrintMatrix(matr);
 
-int[,] FillMatrix(int rowsCount, int columsCount, int leftRange, int rightRange)
+double[,] FillMatrix(int rowsCount, int columsCount)
 {
-    int[,] matrix = new int[rowsCount, columsCount];
+    double[,] matrix = new double[rowsCount, columsCount];
     Random rand = new Random();
 
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
       for(int j=0; j < matrix.GetLength(1); j++)
       {
-        matrix[i,j] = rand.Next(leftRange, rightRange+1);
+        matrix[i,j] = Math.Round(new Random().NextDouble()*10,1);
       }
     }
     return matrix;
 }
 
-void PrintMatrix(int[,] matrix)
+void PrintMatrix(double[,] matrix)
 {
      for (int i = 0; i < matrix.GetLength(0); i++)
     {
